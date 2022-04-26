91c3c714c31434c0b2aaeb6f205196c986724241
Silvia-Du <silviadu@hotmail.it>
2022-04-26 14:28:02 +0200

    definiti step di lavoro progressivo (prvvisorio)

--- js/script.js
+++ js/script.js
@@ -5,4 +5,19 @@ Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto p
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
+//creo in js l'area gioco e le card al premersi del pulsante
+
 //prompt deve partire dopo 5 secondi
+//il prompt chiede i numeri, che vengono salvati in un array.
+
+//parte tutto con un pulsante, che deve sparire quando parte tutto.
+//ci deve essere un generatore di 5 numeri casuali
+//devo assegnare i numeri alle card
+//li stampo nelle card
+//mi serve un'array che raccolga i numeri delle card
+//confronto i numeri dell'utente con quelli randomici
+//se sono giusti le card indovinate si illuminano
+//esce una scritta con la quantità dei numeri corrispondenti indovinati + 
+//un pulsante che chiede se si vuole ripartire.
+
+
